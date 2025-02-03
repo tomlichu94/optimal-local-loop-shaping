6158fe402699c303335d4c6b086788eb9d32e455
tomlichu94 <93402687+tomlichu94@users.noreply.github.com>
2025-02-03 10:18:39 -0800

    added data
    - added data runs (4 - 6) have phase correction
    -slx model corrected with a phase delay

--- MATLAB/Data Processing/signal_reconstruction.m
+++ MATLAB/Data Processing/signal_reconstruction.m
@@ -21,17 +21,50 @@ f_in = 8;
 
 %% run hardware, then export the data exporting data
 addpath('Experimental Runs')
-load run_1.mat
-y_encoder = squeeze(out_encoder.signals.values);
-t_encoder = out_encoder.time;
-y_w = out_W.signals.values;
-t_w = out_W.time;
-u_w_FIR = squeeze(in_W.signals.values);
-t_u = squeeze(in_W.time);
+load run_4_phase_correction.mat
+y_encoder = squeeze(out_encoder.signals.values)';
+t_encoder = out_encoder.time';
+d_ss = squeeze(in_W.signals.values)';
+t_ss = in_W.time';
+y_w = out_W.signals.values';
+t_w = out_W.time';
+u_w_FIR = squeeze(in_W.signals.values)';
+t_u = squeeze(in_W.time)';
 figure
 stairs(t_encoder,y_encoder)
 hold on
-stairs(t_w,y_w(:,1))
-stairs(t_w,y_w(:,2))
+stairs(t_w,y_w(1,:))
+stairs(t_w,y_w(2,:))
 xlim([3 5])
 legend('Encoder','FIR','IIR')
+hold off
+
+%% test with built-in function
+close all
+addpath('Functions')
+[dest_fir dest_iir] = signal_recovery(w_k_fir,w_k_iir,B_para,L_t,d_ss);
+
+figure
+stairs(t_w,y_w(1,:))
+hold on
+stairs(t_w,dest_fir)
+xlim([3 5])
+legend('Simulink FIR','MATLAB FIR')
+hold off
+
+figure
+stairs(t_w,y_w(2,:))
+hold on
+stairs(t_w,dest_iir)
+xlim([3 5])
+legend('Simulink IIR','MATLAB IIR')
+hold off
+
+figure
+stairs(t_w,y_encoder)
+hold on
+stairs(t_w,dest_iir)
+stairs(t_w,y_w(2,:))
+xlim([3 5])
+legend('Simulink Enconder','MATLAB IIR','Simulink IIR')
+hold off
